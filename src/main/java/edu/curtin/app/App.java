342c6527d3df8c2fbaa10106bbbdfb762ff0d59c
Sanuda124 <sanuda.warampathi@gmail.com>
2026-09-06 12:53:19 +0530

    connect app to file

--- src/main/java/edu/curtin/app/App.java
+++ src/main/java/edu/curtin/app/App.java
@@ -4,10 +4,23 @@ package edu.curtin.app;
  * Entry point into the application. To change the package, and/or the name of this class, make
  * sure to update the 'mainClass = ...' line in build.gradle.kts.
  */
+import java.io.IOException;
+
 public class App
 {
     public static void main(String[] args)
     {
-        System.out.println(Hello.getHello());
+        Filedata file = new Filedata();
+
+        try{
+            Wbs wbs = file.load (args[0]);
+            System.out.println(wbs.getRoots().size());
+        }
+        catch(java.io.IOException e)
+        {
+            System.out.println(e.getMessage());
+        }
+
+
     }
 }
